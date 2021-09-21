aae898b6025a63c0eb79d5203ac469577d73beef
Jeff Clary <jeffclary@gmail.com>
2021-09-21 16:02:32 -0500

    testing typescript changes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,8 @@
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 import Head from 'next/head'
 import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 import { GetStaticProps, GetStaticPaths } from 'next'
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
